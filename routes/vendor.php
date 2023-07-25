ffc6f683525b53be7424283ad460e50f237ee42c
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-07-25 02:06:11 +0100

    Profile Update- Vendor Profile Update

--- routes/vendor.php
+++ routes/vendor.php
@@ -1,7 +1,11 @@
 <?php 
 use App\Http\Controllers\Backend\VendorController;
+use App\Http\Controllers\Backend\VendorProfileController;
 use Illuminate\Support\Facades\Route;
 
 /* VENDOR ROUTES **/
 Route::get('dashboard' ,[VendorController::class,'dashboard'])->name('dashboard');
+Route::get('profile' ,[VendorProfileController::class,'index'])->name('profile');
+Route::put('profile/update' ,[VendorProfileController::class,'updateProfile'])->name('profile.update');
+Route::put('profile/update/password' ,[VendorProfileController::class,'updatePassword'])->name('update.password');
 
