965cc2d0138c09072470ea5d84b6ce252f898328
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-07-31 14:21:00 +0100

    Slider- Some fixings and improvement in CRUD

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h1>Table</h1>
-            <div class="section-header-breadcrumb">
+            <h1>Slider</h1>
+            {{-- <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                 <div class="breadcrumb-item"><a href="#">Components</a></div>
                 <div class="breadcrumb-item">Table</div>
-            </div>
+            </div> --}}
         </div>
 
         <div class="section-body">
@@ -19,7 +19,8 @@
                         <div class="card-header">
                             <h4>simple table</h4>
                             <div class="card-header-action">
-                                <a href="{{ route('admin.slider.create') }}" class="btn btn-primary">+ Create New</a>
+                                <a href="{{ route('admin.slider.create') }}" class="btn btn-primary"><i
+                                        class="fas fa-plus-circle"></i> Create New</a>
                             </div>
                         </div>
                         <div class="card-body">
