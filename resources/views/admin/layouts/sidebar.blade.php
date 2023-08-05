781be6f0a72af8df3cf2ab0eb4a5f0a5ff1ff15f
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-08-05 14:33:17 +0100

    Category- Working with category

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,29 +1,35 @@
 <div class="main-sidebar sidebar-style-2">
-  <aside id="sidebar-wrapper">
-    <div class="sidebar-brand">
-      <a href="index.html">Stisla</a>
-    </div>
-    <div class="sidebar-brand sidebar-brand-sm">
-      <a href="index.html">St</a>
-    </div>
-    <ul class="sidebar-menu">
-      <li class="menu-header">Dashboard</li>
-      <li class="dropdown active">
-        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-        <ul class="dropdown-menu">
-          <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-          <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-        </ul>
-      </li>
-      <li class="menu-header">Starter</li>
-      <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Manage Web Site</span></a>
-        <ul class="dropdown-menu">
-          <li><a class="nav-link" href="{{route('admin.slider.index')}}">Slider</a></li>        
-        </ul>
-      </li>
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="dropdown active">
+                <a href="{{ route('admin.dashboard') }}" class="nav-link "><i
+                        class="fas fa-fire"></i><span>Dashboard</span></a>
 
-      {{-- <li class="menu-header">Starter</li>
+            </li>
+            <li class="menu-header">Starter</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>Manage Category</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{ route('admin.category.index') }}">Category</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>Manage Web Site</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{ route('admin.slider.index') }}">Slider</a></li>
+                </ul>
+            </li>
+
+            {{-- <li class="menu-header">Starter</li>
       <li class="dropdown">
         <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
         <ul class="dropdown-menu">
@@ -32,8 +38,8 @@
           <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
         </ul>
       </li> --}}
-      {{-- <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li> --}}
-    </ul>
+            {{-- <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li> --}}
+        </ul>
 
-  </aside>
+    </aside>
 </div>
--- routes/admin.php
+++ routes/admin.php
@@ -1,19 +1,24 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SliderController;
 use Illuminate\Support\Facades\Route;
 
 /* ADMIN ROUTES **/
-Route::get('dashboard' ,[AdminController::class,'dashboard'])->name('dashboard');
+
+Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
 
 /** Profile Routes */
-Route::get('profile',[ProfileController::class,'index'])->name('profile');
-Route::post('profile/update',[ProfileController::class,'updateProfile'])->name('profile.update');
-Route::post('profile/update/password',[ProfileController::class,'updatePassword'])->name('update.password');
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
+Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('update.password');
 
 
 /** SLIDE ROUTES */
 
-Route::resource('slider' ,SliderController::class);
+Route::resource('slider', SliderController::class);
+
+/**  CATEGORY ROUTES */
+Route::resource('category', CategoryController::class);
