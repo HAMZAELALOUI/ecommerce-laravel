0e4ad1d57dbea37592a748939cf4f46a8dd4eace
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-08-06 12:46:33 +0100

    Subcategory- working with index page

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SliderController;
+use App\Http\Controllers\Backend\SubCategoryController;
 use Illuminate\Support\Facades\Route;
 
 /* ADMIN ROUTES **/
@@ -23,3 +24,6 @@ Route::resource('slider', SliderController::class);
 /**  CATEGORY ROUTES */
 Route::put('change-status', [CategoryController::class, 'changeStatus'])->name('category.change-status');
 Route::resource('category', CategoryController::class);
+/**  SUBCATEGORY ROUTES */
+Route::put('sub-category/change-status', [CategoryController::class, 'changeStatus'])->name('sub-category.change-status');
+Route::resource('sub-category', SubCategoryController::class);
