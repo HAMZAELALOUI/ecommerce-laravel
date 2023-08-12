6ae33f2027b8cb537a8ca09c1e62f67e50f1f2b1
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-08-12 23:41:52 +0100

    Make sidebar item active depending on page

---
+++ app/Helpers/helper.php
@@ -0,0 +1,14 @@
+<?php
+
+/**set side bar item active */
+
+function setActive(array $route)
+{
+  if (is_array($route)) {
+    foreach ($route as $r) {
+      if (request()->routeIs($r)) {
+        return 'active';
+      }
+    }
+  }
+}
