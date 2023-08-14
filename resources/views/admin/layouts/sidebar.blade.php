41c774847134c0f69aaf7bf4178e33c1c5deed7e
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2023-08-14 12:12:44 +0100

    Brand- creating necessary files for CRUD

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -27,6 +27,14 @@
                             href="{{ route('admin.child-category.index') }}">child Category</a></li>
                 </ul>
             </li>
+            <li class="dropdown {{ setActive(['admin.brand.*']) }}">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>Manage Web Site</span></a>
+                <ul class="dropdown-menu">
+                    <li class="{{ setActive(['admin.brand.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.brand.index') }}">Slider</a></li>
+                </ul>
+            </li>
             <li class="dropdown {{ setActive(['admin.slider.*']) }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Manage Web Site</span></a>
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ChildCategoryController;
 use App\Http\Controllers\Backend\ProfileController;
@@ -37,3 +38,8 @@ Route::get('get-subcategory', [ChildCategoryController::class, 'getSubCategory']
 Route::get('get-subcategory-edit', [ChildCategoryController::class, 'getSubCategoryEdit'])->name('get-subcategory-edit');
 Route::put('child-category/change-status', [ChildCategoryController::class, 'changeStatus'])->name('child-category.change-status');
 Route::resource('child-category', ChildCategoryController::class);
+
+
+/**  Brand ROUTES */
+Route::put('brand/change-status', [BrandController::class, 'changeStatus'])->name('brand.change-status');
+Route::resource('brand', BrandController::class);
